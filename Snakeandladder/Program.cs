7b08f838f1de9487109ee7eb06d90e16cfce588a
Omkarbade <omakrbade3799@gmail.com>
2022-01-18 20:47:06 +0530

    Adding StartPosition program

---
+++ Snakeandladder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakeandladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder program");
+            StartPosition StartPosition = new StartPosition();
+            StartPosition.StartGame();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Snakeandladder/StartPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakeandladder
+{
+    public class StartPosition
+    {
+        // Instance variable
+        int position = 0;
+        Random random = new Random(); //Calling object class  
+
+        public void StartGame() //Calling Method
+        {
+            Console.WriteLine("Player position is: " + position);
+            int RandomNum = random.Next(1, 7); //Generating random number between 0 and 6
+            Console.WriteLine("randomNum: " + RandomNum);
+        }
+    }
+}
