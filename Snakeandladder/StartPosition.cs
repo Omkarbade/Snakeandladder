d0c72cab2ecad63294dfc9378c41be0c602203b9
Omkarbade <omakrbade3799@gmail.com>
2022-01-19 00:28:11 +0530

    Adding DiceCount Program

--- Snakeandladder/StartPosition.cs
+++ Snakeandladder/StartPosition.cs
@@ -13,26 +13,38 @@ namespace Snakeandladder
         const int MaxPosition = 100;
         const int LADDER = 1;
         const int SNAKE = 2;
-        Random random = new Random(); // object of random class
+        Random random = new Random();       // object of random class
         //Method
         public void StartGame()
         {
             Console.WriteLine("Player position is: " + position);
-            int DiceValue = random.Next(1, 7);          //random number berween 1 to 6 for Dice Value.
-            int playCheck = random.Next(0, 3);          //Generating random value between 0 to 2 to control movement of player position
-            switch (playCheck)
+            //Using While Loop to iterate Game till position reaches max position.
+            int DiceCount = 0;
+            while (position < MaxPosition)
             {
-                case LADDER:
-                    Console.WriteLine("Move player position ahead of Dice Value ");
-                    break;
-                case SNAKE:
-                    Console.WriteLine("Move player position behind the Dice Value ");
-                    break;
-                default:
-                    Console.WriteLine("player will stays at his position ");
-                    break;
+                int DiceValue = random.Next(1, 7);          //random number berween 1 to 6 for Dice Value.
+                int playCheck = random.Next(0, 3);          //Generating random value between 0 to 2 to control movement of player position
+                switch (playCheck)
+                {
+                    case LADDER:
+                        if (position <= MaxPosition && (position + DiceValue) <= MaxPosition)
+                        {
+                            position += DiceValue;
+                        }
+                        break;
+                    case SNAKE:
+                        if (position > 0 && (position - DiceValue) >= 0)
+                        {
+                            position -= DiceValue;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                DiceCount++;
+                Console.WriteLine("Dice Roll:{0} and Player Position:{1}", DiceCount, position);
             }
+            Console.WriteLine("\nNumber of Dice played: " + DiceCount + "\tPlayer Position: " + position);
         }
-        
     }
 }
