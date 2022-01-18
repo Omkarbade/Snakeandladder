8cebbbcf120ec8a1ae7d8665076b4b600b723bab
Omkarbade <omakrbade3799@gmail.com>
2022-01-19 00:01:00 +0530

    adding

--- Snakeandladder/StartPosition.cs
+++ Snakeandladder/StartPosition.cs
@@ -8,15 +8,31 @@ namespace Snakeandladder
 {
     public class StartPosition
     {
-        // Instance variable
+        //Instance Variables.
         int position = 0;
-        Random random = new Random(); //Calling object class  
-
-        public void StartGame() //Calling Method
+        const int MaxPosition = 100;
+        const int LADDER = 1;
+        const int SNAKE = 2;
+        Random random = new Random(); // object of random class
+        //Method
+        public void StartGame()
         {
             Console.WriteLine("Player position is: " + position);
-            int RandomNum = random.Next(1, 7); //Generating random number between 0 and 6
-            Console.WriteLine("randomNum: " + RandomNum);
+            int DiceValue = random.Next(1, 7);          //random number berween 1 to 6 for Dice Value.
+            int playCheck = random.Next(0, 3);          //Generating random value between 0 to 2 to control movement of player position
+            switch (playCheck)
+            {
+                case LADDER:
+                    Console.WriteLine("Move player position ahead of Dice Value ");
+                    break;
+                case SNAKE:
+                    Console.WriteLine("Move player position behind the Dice Value ");
+                    break;
+                default:
+                    Console.WriteLine("player will stays at his position ");
+                    break;
+            }
         }
+        
     }
 }
